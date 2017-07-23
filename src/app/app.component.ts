8d1d1d9a68e952df370c6920adcb36363a0de7ad
jay <jay.lecorse@gmail.com>
2017-07-23 14:28:53 +0200

    Google Maps and Admob bug  commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AdMobFree, AdMobFreeBannerConfig } from '@ionic-native/admob-free';
 
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -11,12 +12,40 @@ import { TabsPage } from '../pages/tabs/tabs';
 export class MyApp {
   rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen   ,  private admobFree: AdMobFree
+) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+    this.showAdmobBannerAds();
+
+
     });
+
+
   }
+
+  showAdmobBannerAds(){
+
+        const bannerConfig: AdMobFreeBannerConfig = {
+ // add your config here
+ // for the sake of this example we will just use the test config
+
+ isTesting: true,
+ autoShow: true
+};
+
+this.admobFree.banner.config(bannerConfig);
+
+       
+ 
+     this.admobFree.banner.prepare()
+  .then(() => {
+    // banner Ad is ready
+    // if we set autoShow to false, then we will need to call the show method here
+  })
+  .catch(e => console.log(e));
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { AdMobFree, AdMobFreeBannerConfig } from '@ionic-native/admob-free';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
@@ -10,6 +11,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { GoogleMaps } from '@ionic-native/google-maps';
 
 @NgModule({
   declarations: [
@@ -34,6 +36,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    AdMobFree,
+    GoogleMaps,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
